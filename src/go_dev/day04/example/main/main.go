5286c510eb740637694edcf27c58fcc8c4f21840
inscode <dongdong11019@gmail.com>
2019-02-27 23:06:37 +0800

    'dev'

--- src/go_dev/day04/example/main/main.go
+++ src/go_dev/day04/example/main/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-func main()  {
+func main() {
 	fmt.Println("test func new")
 	demo_04.NewCode()
 
@@ -17,4 +17,15 @@ func main()  {
 
 	fmt.Println("test func closure")
 	demo_04.Closure()
+
+	fmt.Println("--------------------------")
+
+	s1 := make([]int, 10)
+	s1 = []int{1, 2, 3, 4, 5, 6, 6, 7}
+	fmt.Println(s1)
+
+	s2 := s1[:len(s1)]
+	fmt.Println(s2)
+
+	demo_04.TestSlice()
 }
