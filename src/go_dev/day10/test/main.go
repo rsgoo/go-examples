ee30b0fdb9b30dc2049478b8df02e77a715e03d5
liudong <liudong@mafengwo.com>
2019-06-13 22:06:16 +0800

    dev lko

--- src/go_dev/day10/test/main.go
+++ src/go_dev/day10/test/main.go
@@ -1,14 +1,17 @@
 package main
 
-import (
-	"reflect"
-	"fmt"
-)
+import "fmt"
+
+func sum(n1 int, n2 int) (int) {
+	//defer 是先进后出的顺序
+	defer fmt.Println("ok1 n1 = ", n1) //execute step 2
+	//defer fmt.Println("ok2 n2 = ", n2) //execute step 1
+	res := n1 + n2
+	fmt.Println("sum is  ", res) //execute step 0
+	return res
+}
 
 func main() {
-	var str = "hello"
-	fs := reflect.ValueOf(&str)
-	fs.Elem().SetString("world")
-	fmt.Printf("%v\n", fs)		//0xc42000e1e0
-	fmt.Printf("%v\n", str)		//world
+	result := sum(11, 22)
+	fmt.Println(result) //execute step 3
 }
--- src/node.go
+++ src/node.go
@@ -2,19 +2,64 @@ package main
 
 import (
 	"fmt"
-	"runtime"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/astaxie/beego/httplib"
+	"io"
+	"log"
+	"net/http"
 )
 
-func add(a, b int, pipe chan int) {
-	sum := a + b
-	pipe <- sum
+
+const HOST = "https://www.codecasts.com/"
+const LOGIN_PAGE_URL = "user/login?redirect_url=https://www.codecasts.com"
+const LOGIN_POST_URL = "user/login"
+const USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+
+var myCookie = []*http.Cookie{}
+var myHeader = map[string]string{
+	"User-Agent": USER_AGENT,
 }
 
 func main() {
-	var pipe chan int
-	pipe = make(chan int, 1)
-	go add(11, 22, pipe)
-	sum := <-pipe
-	fmt.Println("sum from pipe is ", sum)
-	fmt.Println(runtime.GOMAXPROCS(runtime.NumCPU()))
+
+	GetLoginInfo()
+
+
+}
+
+func PostLoginInfo() {
+	//url := HOST + LOGIN_POST_URL
+}
+
+func GetLoginInfo() {
+	url := HOST + LOGIN_PAGE_URL
+
+	req := httplib.Get(url)
+
+	resp, err := req.SetUserAgent(USER_AGENT).Response()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	myCookie = resp.Cookies()
+
+	respBytes, err := req.Bytes()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(resp,respBytes)
+
+	document, err := goquery.NewDocumentFromReader()
+
+	fmt.Println(document)
+}
+
+func CookieSliceToString(oriCookie []string) string {
+
+	str := ""
+	for _, v := range oriCookie {
+		str += v + ";"
+	}
+	return str
 }
