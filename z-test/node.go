92edf50156a4b10d4c10b0f1c6b042c34d302842
inscode <dongdong11019@gmail.com>
2020-06-20 17:26:18 +0800

    dev log

--- z-test/node.go
+++ z-test/node.go
@@ -1,18 +1,2 @@
 package main
 
-import "fmt"
-
-type student struct {
-	id   int
-	name string
-	age  int
-}
-
-func main() {
-	ce := make(map[int]student)
-	ce[1] = student{12, "xiaolizi", 22}
-	ce[2] = student{13, "wang", 23}
-	fmt.Println(ce)
-	delete(ce, 2)
-	fmt.Println(ce)
-}
